0c18045c745a44dd1077ec718305d2356d401a49
Davi Sandro <davisandro300@gmail.com>
2022-05-10 19:28:38 -0300

    inicio do estudo Basico

---
+++ Estudos_NodeJS/Basico_node/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const userRoute = require('./routes/userRoutes')
+
+const userRoutes = require('./routes/userRoutes')
+
+const app = express()
+const port =3000
+
+userRoute(app)
+
+app.port('/', (req ,res) => res.send('Olá mundo pelo na porta 3000'))
---
+++ Estudos_NodeJS/Basico_node/routes/userRoutes.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const{ join} = fs
+
+const filePath = join(__dirname, 'user.json')
+
+const getUsers = () => {
+    const data = fs.readFileSync(filePath)
+        ? fs.readFileSync(filePath)
+        : []
+    try{
+        return JSON.parse(data)
+    }catch (error) {
+        return[]
+    }
+}
+
+const saveUser = (users) => fs.
+        writeFileSync(filePath, JSON.stringify(users, null, '\t'))
+
+const userRoute = (app) => {
+    app.route('/users/id?')
+        .get((req, res) => {
+            const users = getUsers()
+
+            res.send({ users })
+        })
+}
+
+module.exports = userRoute
